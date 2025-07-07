b9d6e77fc3806a8cac67e9da9b0e0130e56bb1c6
Ranveer1010 <ranveer.nagar2011@gmail.com>
2025-07-06 21:56:23 -0700

    Code till end of chapter 3

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Learning/LearningJava.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.FTC_Learning;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class LearningJava extends OpMode{
+    int gradeForGradeSeven = 98;
+
+    @Override
+    public void init(){
+        telemetry.addLine("Hello World!");
+    }
+    @Override
+    public void start(){
+        telemetry.addLine("Starting Opmode");
+        telemetry.addData("Your Grade Is: " , gradeForGradeSeven);
+    }
+
+    @Override
+    public void loop(){
+        double speed = gamepad1.left_stick_y / 2.0;
+        telemetry.addData("Right stick X GP1 = " , gamepad1.right_stick_x);
+        telemetry.addData("Right stick Y GP1 = " , gamepad1.right_stick_y);
+        telemetry.addData("Diff between GP1 left and right joystick y is " , gamepad1.left_stick_y - gamepad1.right_stick_y);
+        telemetry.addData("Sum of GP1 two triggers is " , gamepad1.left_trigger + gamepad1.right_trigger);
+        telemetry.addData("Speed is ", speed);
+        if (gamepad1.b){
+            telemetry.addLine("B is pressed");
+        }
+    }
+}
