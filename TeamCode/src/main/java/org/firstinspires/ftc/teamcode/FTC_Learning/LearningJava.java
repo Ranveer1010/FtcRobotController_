3299c31ffe92deaafc54fd208d56a9236eec6123
Ranveer1010 <ranveer.nagar2011@gmail.com>
2025-07-12 19:35:39 +0530

    Cleaning up code with methods and better looops.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Learning/LearningJava.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Learning/LearningJava.java
@@ -5,6 +5,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class LearningJava extends OpMode{
     int gradeForGradeSeven = 98;
+    void robotSpeed(){
+        double speed = -1.0 * gamepad1.left_stick_y;
+        telemetry.addData("Bot speed is: " , speed);
+    }
+    void triggersAdded(){
+        double trigger1 = gamepad1.left_trigger;
+        double trigger2 = gamepad1.right_trigger;
+        double triggersTogether = trigger1 + trigger2;
+        telemetry.addData("Triggers added is: " , triggersTogether);
+    }
+    void turtleMode(){
+
+        while(gamepad1.a){
+            double gp1StickYTurtle = gamepad1.right_stick_y/2.0;
+            double gp1StickXTurtle = gamepad1.right_stick_x/2.0;
+            telemetry.addLine("Turtle active");
+            telemetry.addData("GP1 Y = " , gp1StickYTurtle);
+            telemetry.addData("GP1 X = " , gp1StickXTurtle);
+
+        }
+    }
 
     @Override
     public void init(){
@@ -18,29 +39,14 @@ public class LearningJava extends OpMode{
 
     @Override
     public void loop(){
-        double speed = gamepad1.left_stick_y / 2.0;
+    robotSpeed();
+    triggersAdded();
+    turtleMode();
         telemetry.addData("Right stick X GP1 = " , gamepad1.right_stick_x);
         telemetry.addData("Right stick Y GP1 = " , gamepad1.right_stick_y);
         telemetry.addData("Diff between GP1 left and right joystick y is " , gamepad1.left_stick_y - gamepad1.right_stick_y);
-        telemetry.addData("Sum of GP1 two triggers is " , gamepad1.left_trigger + gamepad1.right_trigger);
-        telemetry.addData("Speed is ", speed);
         if (gamepad1.b){
             telemetry.addLine("B is pressed");
-        }
-        if(gamepad1.a){
-            gamepad1.right_stick_y = gamepad1.right_stick_y/1;
-            gamepad1.right_stick_x = gamepad1.right_stick_x/1;
-        }
-        else{
-            gamepad1.right_stick_y = gamepad1.right_stick_y/2;
-            gamepad1.right_stick_x = gamepad1.right_stick_x/2;
-        }
-        //crazy mode:
-        if(gamepad1.left_bumper){
-            gamepad1.left_stick_y = gamepad1.left_stick_x;
-            gamepad1.left_stick_x = gamepad1.left_stick_y;
-            gamepad1.right_stick_x = gamepad1.right_stick_y;
-            gamepad1.right_stick_y = gamepad1.right_stick_x;
         }
     }
 }
