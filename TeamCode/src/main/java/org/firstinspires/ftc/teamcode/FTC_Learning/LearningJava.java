17665b5fc902f769c8545094c820a3fc4bee43c4
Ranveer1010 <ranveer.nagar2011@gmail.com>
2025-07-14 14:26:00 +0530

    Polishing code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Learning/LearningJava.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Learning/LearningJava.java
@@ -5,9 +5,18 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class LearningJava extends OpMode{
     int gradeForGradeSeven = 98;
+
     void robotSpeed(){
-        double speed = -1.0 * gamepad1.left_stick_y;
-        telemetry.addData("Bot speed is: " , speed);
+        if(gamepad1.a){
+            double turtleInput = gamepad1.left_stick_y/-2.0;
+            telemetry.addLine("Turtle active");
+            telemetry.addData("Turtle speed is: " , turtleInput);
+        }
+        else{
+            telemetry.addLine("Turtle not active robot at normal speed");
+            double speed = -1.0 * gamepad1.left_stick_y;
+            telemetry.addData("Bot speed is: " , speed);
+        }
     }
     void triggersAdded(){
         double trigger1 = gamepad1.left_trigger;
@@ -15,18 +24,6 @@ public class LearningJava extends OpMode{
         double triggersTogether = trigger1 + trigger2;
         telemetry.addData("Triggers added is: " , triggersTogether);
     }
-    void turtleMode(){
-
-        while(gamepad1.a){
-            double gp1StickYTurtle = gamepad1.right_stick_y/2.0;
-            double gp1StickXTurtle = gamepad1.right_stick_x/2.0;
-            telemetry.addLine("Turtle active");
-            telemetry.addData("GP1 Y = " , gp1StickYTurtle);
-            telemetry.addData("GP1 X = " , gp1StickXTurtle);
-
-        }
-    }
-
     @Override
     public void init(){
         telemetry.addLine("Hello World!");
@@ -41,7 +38,6 @@ public class LearningJava extends OpMode{
     public void loop(){
     robotSpeed();
     triggersAdded();
-    turtleMode();
         telemetry.addData("Right stick X GP1 = " , gamepad1.right_stick_x);
         telemetry.addData("Right stick Y GP1 = " , gamepad1.right_stick_y);
         telemetry.addData("Diff between GP1 left and right joystick y is " , gamepad1.left_stick_y - gamepad1.right_stick_y);
