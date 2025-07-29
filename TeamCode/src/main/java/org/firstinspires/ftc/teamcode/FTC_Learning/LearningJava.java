7380e6bde99ffe5186f3bb53befc556395124598
Ranveer1010 <ranveer.nagar2011@gmail.com>
2025-07-29 13:04:27 -0700

    Forward motor working. setPivot method done but untested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Learning/LearningJava.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Learning/LearningJava.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.FTC_Learning;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Mechanisms.Config;
@@ -12,14 +13,23 @@ public class LearningJava extends OpMode {
     //Var creation
     int gradeForGradeSeven = 98;
     boolean initDone;
+    double speed;
 
     //Object creation
     RobotLocation robotLocation = new RobotLocation(0); //Sets start angle to 0
     Config config = new Config();
 
+    double squareInputWithSign(double input) {
+        double output = input * input;
+        if (input < 0) {
+            output = output * -1.0;
+        }
+        return output;
+    }
+
     //Get robot speed method
      void robotSpeed() {
-        if (gamepad1.a) {
+        if (gamepad1.left_stick_button) {
             double turtleInput = gamepad1.left_stick_y / -2.0;
             telemetry.addLine("Turtle active");
             telemetry.addData("Turtle speed is: ", turtleInput);
@@ -30,26 +40,45 @@ public class LearningJava extends OpMode {
         }
     }
 
-    //Drive method
-    void forwardDrive(){
-        if(gamepad1.a){
-            config.setRightFront(gamepad1.left_stick_y);
-            config.setRightBack(gamepad1.left_stick_y);
-            config.setLeftFront(gamepad1.left_stick_y);
-            config.setLeftBack(gamepad1.left_stick_y);
+    void slideControl() {
+        if (gamepad1.b) {
+            config.setSlideExtend(gamepad1.right_stick_y / 2.5);
         }
         else{
-            config.setRightFront(0.0);
-            config.setRightBack(0.0);
-            config.setLeftFront(0.0);
-            config.setLeftBack(0.0);
+            config.setSlideExtend(0.0);
         }
     }
-    void slideControl() {
-        if (gamepad1.right_stick_y > 0) {
-            config.setSlideExtend(-gamepad1.right_stick_y / 2.5);
+
+    void driveForward(){
+        if (gamepad1.a) {
+            // Show on telemetry that button A is being held
+            telemetry.addLine("A is running new new new new");
+
+            // Get average encoder position of left side motors
+            int leftPos = (config.getLeftFront().getCurrentPosition() + config.getLeftBack().getCurrentPosition()) / 2;
+
+            // Get average encoder position of right side motors
+            int rightPos = (config.getRightFront().getCurrentPosition() + config.getRightBack().getCurrentPosition()) / 2;
+
+            // Calculate the error (difference) between left and right side
+            int error = leftPos - rightPos;
+
+            // Apply a correction factor to reduce drifting/curving
+            double correction = error * 0.001;  // You can tune this value
+
+            // Get forward/backward input from joystick (negative to correct direction)
+            double basePower = gamepad1.left_stick_y;
+
+            // Adjust motor power to keep the bot driving straight
+            config.setAllDrivePower(basePower - correction, basePower + correction);
+        } else {
+            // If A is not pressed, stop all motors
+            config.setAllDrivePower(0, 0);
         }
+
     }
+
+
 
     //Trigger addition method
     void triggersAdded() {
@@ -65,6 +94,8 @@ public class LearningJava extends OpMode {
         initDone = true;
         telemetry.addData("Init done = ", initDone);
         config.init(hardwareMap);
+        config.getPivot().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        config.getPivot().setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
     }
 
@@ -78,10 +109,17 @@ public class LearningJava extends OpMode {
     @Override
     public void loop() {
 
+        speed = gamepad1.left_stick_y * -1.0;
+
+
         robotSpeed();
         triggersAdded();
-        forwardDrive();
+        driveForward();
         slideControl();
+
+        if(gamepad1.left_bumper){
+            config.setPivot(134 , 0.5);
+        }
 
         /*String touchSensorPressed = "Not pressed";
         if(touchSensor.isTouchSensorPressed()){
@@ -94,8 +132,8 @@ public class LearningJava extends OpMode {
         telemetry.addData("Right stick Y GP1 = ", gamepad1.right_stick_y);
         telemetry.addData("Diff between GP1 left and right joystick y is ", gamepad1.left_stick_y - gamepad1.right_stick_y);
 
-        if (gamepad1.b) {
-            telemetry.addLine("B is pressed");
+        if(gamepad1.right_stick_button){
+            config.setZeroPower(DcMotor.ZeroPowerBehavior.BRAKE);
         }
 
         if (gamepad1.x) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Config.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Mechanisms;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Config {
@@ -20,33 +21,56 @@ public class Config {
         pivot = hwMap.get(DcMotor.class , "pivot");
         slideExtend = hwMap.get(DcMotor.class , "slide_extend");
 
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
-
         rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER); //Switch to RUN_TO_POS in tele/auto
+        pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         slideExtend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
 
         ticksPerRotation = rightFront.getMotorType().getTicksPerRev();
     }
-    public void setRightFront(double speed){
-        rightFront.setPower(speed);
-    }public void setRightBack(double speed){
-        rightBack.setPower(speed);
-    }public void setLeftFront(double speed){
-        leftFront.setPower(speed);
-    }public void setLeftBack(double speed){
-        leftBack.setPower(speed);
-    }public void setPivot(int pos, double power){
+
+    public void setZeroPower(DcMotor.ZeroPowerBehavior zeroPowerBehavior){
+        rightFront.setZeroPowerBehavior(zeroPowerBehavior);
+        rightBack.setZeroPowerBehavior(zeroPowerBehavior);
+        leftFront.setZeroPowerBehavior(zeroPowerBehavior);
+        leftBack.setZeroPowerBehavior(zeroPowerBehavior);
+    }
+
+    public void setAllDrivePower(double leftPower, double rightPower) {
+        leftFront.setPower(leftPower);
+        leftBack.setPower(leftPower);
+        rightFront.setPower(rightPower);
+        rightBack.setPower(rightPower);
+    }
+
+    public void setPivot(int pos, double power){
         pivot.setTargetPosition(pos);
+        pivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         pivot.setPower(power);
-    }public void setSlideExtend(double speed){
+    }
+
+    public void setSlideExtend(double speed){
         slideExtend.setPower(speed);
     }
+
     public double getRightFrontMotorRotations(){
         return rightFront.getCurrentPosition() / ticksPerRotation;
+    }
+
+    // Getter methods for encoder positions (added)
+    public DcMotor getLeftFront() { return leftFront; }
+    public DcMotor getLeftBack() { return leftBack; }
+    public DcMotor getRightFront() { return rightFront; }
+    public DcMotor getRightBack() { return rightBack; }
+
+    public DcMotor getPivot() {
+        return pivot;
     }
 }
